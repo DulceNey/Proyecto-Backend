36e2347f7f5e2cb97eeb576fe49bb99119e2379d
DulceNey <terapiaintegralney@hotmail.com>
2022-12-23 18:33:32 -0300

    Routes are still on development

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,8 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const { usersRoute } = require('../controllers/userController');
+const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', usersRoute);
 
 module.exports = router;
