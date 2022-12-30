b42158520123d7363e70ec3f85b2c07b91731843
DulceNey <terapiaintegralney@hotmail.com>
2022-12-30 16:30:18 -0300

    Basic Middleware and the inclusion of .Env are working

--- controllers/registeredUsers.js
+++ controllers/registeredUsers.js
@@ -1,24 +1,68 @@
 const {usersData} = require("../models/UsersData");
+const {check, validationResult} = require("express-validator")
 
-//GET
+//GET 
 const registeredUsers = async (req, res) => {
-    const registered = await usersData.find(req.body);
-    res.json({registered})
+    try {
+        const protect = validationResult(req)
+        if (protect.isEmpty) {
+            const registered = await usersData.find(req.body);
+            res.json({registered})
+        } else {
+            res.status(501).json({registered})
+        }
+    } catch (error) {
+        res.status(501).json({error})
+    }
     }
 
 //POST    
 const newUser = async (req, res) => {
-    const users = new usersData (req.body);
-    await users.save();
+    try {
+        const protect = validationResult(req)
+        if (protect.isEmpty) {
+            const users = new usersData (req.body);
+            await users.save();
+            res.status(201).json({registered})
+        } else {
+            res.status(501).json(protect)
+        }
+        
+    } catch (error) {
+        res.status(501).json({error})
+    }
 }
 
 //PUT
 const updateUser = async (req, res) => {
-    await usersData.findByIdAndUpdate(req.params.id, req.body);
+    try {
+        const protect = validationResult(req)
+        if (protect.isEmpty) {
+            await usersData.findByIdAndUpdate(req.params.id, req.body);
+            res.json({registered})
+        } else {
+            res.status(501).json(protect)
+        }
+    } catch (error) {
+        res.status(501).json({error})
+    }
+    
 }
+
 //DELETE
 const deleteUser =  async (req, res) => {
-    await usersData.findByIdAndDelete(req.params.id);
+    try {
+        const protect = validationResult(req)
+        if (protect.isEmpty) {
+            await usersData.findByIdAndDelete(req.params.id);
+            res.json(registered)
+        } else {
+            res.status(501).json(protect)
+        }
+    } catch (error) {
+        res.status(501).json({error})
+    }
+    
 
 }
 
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { usersRoute } = require('../controllers/userController');
 const {registeredUsers, newUser, updateUser, deleteUser} = require("../controllers/registeredUsers");
+const {check} = require("express-validator")
 const router = express.Router();
 
 //GET
@@ -8,12 +9,16 @@ router.get('/', usersRoute);
 router.get("/registered", registeredUsers);
 
 //POST
-router.post("/new", newUser)
+router.post("/new", 
+    [check("Name").not().isEmpty().withMessage("We are sorry, but Name cannot be empty. Dont be Shy.").isLength({min:1 , max: 30}).withMessage("Try again, we can read from 1 to 30 letters in a name"),
+    check("Age").not().isEmpty().withMessage("We need to know you better. Allow us to know your age, please.").isNumeric().withMessage("Tell us your age with numbers"),
+    check("Ocuppation").isLength({min:1 , max: 30}).withMessage("Please, try to use at least 1 word and lesser than 30 of them")]
+,newUser)
 
 //PUT
 router.put("/update/:id", updateUser)
 
 //DELETE
-router.delete("/delete/:id", deleteUser)
+router.delete("/delete/:id",deleteUser)
 
 module.exports = router;
